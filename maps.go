18f9d385de5386e1dff417c2cb9cd232228ff640
Dipesh <dipeshkhawase22@gmail.com>
2018-10-20 18:28:44 +0200

    maps.go

---
+++ maps.go
@@ -0,0 +1,22 @@
+package main
+
+func main()  {
+	//map is a reference type
+	//var scores map[string]float32 //[key]valuetype
+	//or
+	scores := make(map[string]int)//make initialize the map
+	scores["saurabh"] = 70
+	scores["lucas"] = 79
+	scores["dante"] = 77
+
+	loop(scores)
+	delete(scores,"saurabh")
+	loop(scores)
+
+}
+
+func loop(c map[string]int)  {
+	for key,value := range c  {
+		println(key,value)
+	}
+}
